94b832260f0c452f6a964ecbc529f955b83030c8
Eric Yuan <ericyuan_home@MacBook-Pro.local>
2024-10-20 11:33:27 -0500

    update ocr with ollama

--- ollama_test.py
+++ ollama_test.py
@@ -1,8 +1,22 @@
 import ollama
+import json
+import random
+import os
+import pytesseract
+from PIL import Image
+
+image_path = ('menu1.jpg')
+
+img = Image.open(image_path)
+ocr_text = pytesseract.image_to_string(img)
+
+print(ocr_text)
+
 response = ollama.chat(model='llama3.2', messages=[
   {
     'role': 'user',
-    'content': 'Can you read a menu and extract dish names, ingredients, prices, and any special notes such as allergens? Return the results in JSON format as an array of objects like this: [{"name": "Dish Name", "ingredients": "Ingredients", "price": "Price", "notes": "Notes"}]. Menu Text: Aloo Gobi $12.99 Fresh cauliflower and potatoes sauteed with ginger, tomatoes, and spices. Contains dairy.'
+    'content': f'Can you read a menu and extract dish names, ingredients, prices, and any special notes such as allergens? Return the results in JSON format as an array of objects like this: [{{"name": "Dish Name", "ingredients": "Ingredients", "price": "Price", "notes": "Notes"}}]. Menu Text: {ocr_text} Please note that the test menu is in Spanish and it is generated from OCR which contains missing letters or words. Please make sure to handle these cases.'
   },
 ])
+
 print(response['message']['content'])
